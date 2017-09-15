41a57b6423169de4f74a335756992f93f095cf34
ssongs77 <ssongs77@gmail.com>
2017-09-15 12:15:25 +0900

    3.3 Controller를 이용한 웹 페이지 작성 ex 3-10, 3-11

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,18 +1,14 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-	String[] names = { "kim", "lee", "park", "choi", "jo" };
-	String[] mails = { "kim@tuuyano.com", "lee@flower.com", "park@yama", "choi@happy", "jo@baba" };
-
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {
+	@RequestMapping("/")
+	public String index() {
 		
-		return new DataObject(id, names[id], mails[id]);
+		return "index";
 	}
 }
