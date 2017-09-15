857a67c6dc99e7693b37c1da5e7154182d21c1d1
ssongs77 <ssongs77@gmail.com>
2017-09-15 11:36:35 +0900

    ex 3-7 : @RequestMapping (매개변수 전달)

---
+++ MyBootApp/src/main/java/com/tuyano/springboot/MyBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.tuyano.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 스프링 부트 애플리케이션 class라는 것을 나타냄
+public class MyBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootAppApplication.class, args);
+	}
+}
