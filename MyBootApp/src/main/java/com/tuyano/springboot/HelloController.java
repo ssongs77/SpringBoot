c073658d59bdad5b8c13d65c7f1097cdc6f02015
ssongs77 <ssongs77@gmail.com>
2017-09-18 18:11:38 +0900

    ex 3-16, 16 폼 사용하기

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,20 +1,25 @@
 package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
-		int res = 0;
-		for (int i = 1; i < num; i++) {
-			res += i;
-		}
-		mav.addObject("msg", "total : " + res);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "이름을 적어서 전송해 주세요.");
+		return mav;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text") String str, ModelAndView mav) {
+		mav.addObject("msg", "안녕하세요 " + str + " 님!");
+		mav.addObject("value", str);
 		mav.setViewName("index");
 		
 		return mav;
