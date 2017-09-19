0a5f5cbd3c487bff510e11378eda7d264e245b9e
ssongs77 <ssongs77@gmail.com>
2017-09-19 16:56:11 +0900

    4-20 th:each

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,21 +1,24 @@
 package com.tuyano.springboot;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/{month}")
-	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		int m = Math.abs(month) % 12;
-		m = m == 0 ? 12 : m;
-		mav.addObject("month",m);
-		mav.addObject("check", Math.floor(m/3));
-
+		
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"park", "park@yama", "090-111-2222"});
+		data.add(new String[] {"kim", "kim@dddd", "010-123-1234"});
+		data.add(new String[] {"lee", "lee@yama", "090-312-4321"});
+		
+		mav.addObject("data", data);
 		return mav;
 	}
 }
