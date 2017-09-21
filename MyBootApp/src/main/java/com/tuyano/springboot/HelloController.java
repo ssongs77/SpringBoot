e3b03c5d0e2977d85f6951ef5fc3b3311d435a22
ssongs77 <ssongs77@gmail.com>
2017-09-21 19:17:49 +0900

    5-13. 5-14 엔터티 삭제

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -9,18 +9,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tuyano.springboot.repositories.MyDataRrepository;
 
 @Controller
 public class HelloController {
-	
+
 	@Autowired
 	MyDataRrepository repository;
 
 	@PostConstruct
-	public void init(){
+	public void init() {
 		MyData d1 = new MyData();
 		d1.setName("kim");
 		d1.setAge(123);
@@ -40,22 +41,36 @@ public class HelloController {
 		d3.setMemo("my work friend...");
 		repository.saveAndFlush(d3);
 	}
-	
+
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-	public ModelAndView edit(@ModelAttribute MyData mydata, 
-			@PathVariable int id,ModelAndView mav) {
+	public ModelAndView edit(@ModelAttribute MyData mydata, @PathVariable int id, ModelAndView mav) {
 		mav.setViewName("edit");
-		mav.addObject("title","edit mydata.");
-		MyData data = repository.findById((long)id);
-		mav.addObject("formModel",data);
+		mav.addObject("title", "edit mydata.");
+		MyData data = repository.findById((long) id);
+		mav.addObject("formModel", data);
 		return mav;
 	}
 
 	@RequestMapping(value = "/edit", method = RequestMethod.POST)
-	@Transactional(readOnly=false)
-	public ModelAndView update(@ModelAttribute MyData mydata, 
-			ModelAndView mav) {
+	@Transactional(readOnly = false)
+	public ModelAndView update(@ModelAttribute MyData mydata, ModelAndView mav) {
 		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public ModelAndView delete(@PathVariable int id, ModelAndView mav) {
+		mav.setViewName("delete");
+		mav.addObject("title", "delete mydata.");
+		MyData data = repository.findById((long) id);
+		mav.addObject("formModel", data);
+		return mav;
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	@Transactional(readOnly = false)
+	public ModelAndView remove(@RequestParam long id, ModelAndView mav) {
+		repository.delete(id);
 		return new ModelAndView("redirect:/");
 	}
 
