f8a92f15a36656b297c00482976bf706fa65adcf
ssongs77 <ssongs77@gmail.com>
2017-09-21 18:34:05 +0900

    ex 5-12 edit기능 구현

--- MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -40,21 +41,19 @@ public class HelloController {
 		repository.saveAndFlush(d3);
 	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index(
-			@ModelAttribute("formModel") MyData mydata, 
-			ModelAndView mav) {
-		mav.setViewName("index");
-		mav.addObject("msg","this is sample content.");
-		Iterable<MyData> list = repository.findAll();
-		mav.addObject("datalist",list);
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView edit(@ModelAttribute MyData mydata, 
+			@PathVariable int id,ModelAndView mav) {
+		mav.setViewName("edit");
+		mav.addObject("title","edit mydata.");
+		MyData data = repository.findById((long)id);
+		mav.addObject("formModel",data);
 		return mav;
 	}
 
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
 	@Transactional(readOnly=false)
-	public ModelAndView form(
-			@ModelAttribute("formModel") MyData mydata, 
+	public ModelAndView update(@ModelAttribute MyData mydata, 
 			ModelAndView mav) {
 		repository.saveAndFlush(mydata);
 		return new ModelAndView("redirect:/");
