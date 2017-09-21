76fe6798720dcfedd233c911ee1a9bb0657bdcfb
ssongs77 <ssongs77@gmail.com>
2017-09-21 19:19:25 +0900

    5-15. JpaRepository 메서드 구현 예

--- MyBootApp/src/main/java/com/tuyano/springboot/repositories/MyDataRrepository.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/repositories/MyDataRrepository.java
@@ -1,5 +1,7 @@
 package com.tuyano.springboot.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import com.tuyano.springboot.MyData;
 @Repository
 public interface MyDataRrepository extends JpaRepository<MyData, Long> {
 	public MyData findById(Long name);
+	public List<MyData> findByNameLike(String name);
+	public List<MyData> findByIdIsNotNullOrderByIdDesc();
+	public List<MyData> findByAgeGreaterThan(Integer age);
+	public List<MyData> findByAgeBetween(Integer age1, Integer age2);
 }
