93681b1f4f7d551a0038157725968834092c1a55
ssongs77 <ssongs77@gmail.com>
2017-09-27 11:28:36 +0900

    6-21 @Query 파라미터 사용 하기

--- MyBootApp/src/main/java/com/tuyano/springboot/repositories/MyDataRepository.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/repositories/MyDataRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.tuyano.springboot.MyData;
@@ -23,4 +24,7 @@ public interface MyDataRepository extends JpaRepository<MyData, Long> {
 
 	@Query("SELECT d FROM MyData d ORDER BY d.name")
 	List<MyData> findAllOrderByName();
+	
+	@Query("from MyData where age > :min and age < :max")
+	public List<MyData> findByAge(@Param("min") int min, @Param("max")int max);
 }
